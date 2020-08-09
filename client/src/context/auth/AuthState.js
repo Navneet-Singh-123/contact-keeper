f5f48f162a3f2266603c2b4a01d6b1a7c4428f37
ns_01 <snavneet561@gmail.com>
2020-08-09 11:55:08 +0530

    Logout and navbar variation, private routing

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -88,7 +88,7 @@ const AuthState = (props) => {
     }
   };
   // Logout user
-  const logout = () => console.log("Logout");
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
